dace9e7276d147afcb5c551788bed043efe0c9a3
Sahaj <chokshisahaj@gmail.com>
2025-04-15 11:00:48 +0530

    hita

--- index.js
+++
@@ -1,7 +0,0 @@
-
-let username;
-document.getElementById("mybutton").onclick = function(){
-    username =document.getElementById("myinput").value;
-    console.log(username);
-    document.getElementById("myhq").textContent = username;
-}
